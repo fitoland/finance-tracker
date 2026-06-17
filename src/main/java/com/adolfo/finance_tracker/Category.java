718bfdb3c798562d810af8bc9962a34c1aa19b61
fitoland <adolfitorasco2@gmail.com>
2026-06-17 17:57:34 +0200

    feat: add Category and Transaction entities

---
+++ src/main/java/com/adolfo/finance_tracker/Category.java
@@ -0,0 +1,21 @@
+package com.adolfo.finance_tracker;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+}
---
+++ src/main/java/com/adolfo/finance_tracker/Transaction.java
@@ -0,0 +1,25 @@
+package com.adolfo.finance_tracker;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    private String description;
+    private LocalDate date;
+    private String type;
+    @ManyToOne
+    private Category category;
+}
